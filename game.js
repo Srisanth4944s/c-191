fd869efd3fe026985e2cc8f38206e44328919bd5
Srisanth4944s <70645902+Srisanth4944s@users.noreply.github.com>
2022-12-20 18:36:18 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent("game",{
+    schema:{
+        gameState:{
+            type:"string",default:"play"
+        }
+    },
+    init:function(){
+        var duration = 300
+        var timerEl = document.querySelector("#timer")
+        this.startTimer(duration,timerEl)
+    },
+    startTimer:function(duration,timerEl) {
+        
+    }
+})
